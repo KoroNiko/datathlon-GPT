3e7e3ba8d6da242d96de90d43a0bdf3169c32b3d
stratos1st <stratos1st@gmail.com>
2023-02-23 13:10:21 +0100

    Add files via upload

--- notebooks/get_pallets_faces.py
+++ notebooks/get_pallets_faces.py
@@ -26,19 +26,10 @@ dataset_dir='./data/'
 
 
 # Number of colors to be extracted
-NUM_COLORS = 20
-Artwork = pd.read_csv(dataset_dir+'Artwork.csv')
+NUM_COLORS = 10
 
-
-def i1():# helper function for fork printing
-  sys.stdout.write(' ')
-  sys.stdout.flush()
-
-def url2array(url: str):
-    ''' Gets a URL and returns an image as a numpy array '''
-    response = requests.get(url)
-    img = Image.open(io.BytesIO(response.content))
-    return np.array(img)
+with open(dataset_dir+'pickle_portraits', 'rb') as handle:
+    image_list = pickle.load(handle)
 
 def extract_colors(model: KMeans, img: np.ndarray) -> Tuple[np.ndarray, Counter, np.ndarray]:
     ''' Extract the X most common colors from an image with a KMeans model '''
@@ -51,11 +42,10 @@ def extract_colors(model: KMeans, img: np.ndarray) -> Tuple[np.ndarray, Counter,
 
 def get_img_pallete(ids_and_urls):
     img_id = ids_and_urls[0]
-    img_url = ids_and_urls[1]
-    img = url2array(img_url)
+    img = ids_and_urls[1]
     kmeans_model = KMeans(n_clusters=NUM_COLORS, n_init='auto')
 
-    _, cluster_counts, rgb_colors = extract_colors(kmeans_model, img)
+    cluster_labels, cluster_counts, rgb_colors = extract_colors(kmeans_model, img)
     # img_quant = np.reshape(rgb_colors[cluster_labels], (img.shape[0], img.shape[1], 3))
 
     return {
@@ -64,28 +54,19 @@ def get_img_pallete(ids_and_urls):
         'rgb_colors':rgb_colors
     }
 
-def save(dict, dir):
-    df = pd.DataFrame(dict)
-    df.cluster_counts = df.cluster_counts.astype(str).str.lstrip('Counter(').str.rstrip(')')
-    df.rgb_colors = df.rgb_colors.apply(lambda x: str([list(a) for a in x]))
-    df.to_csv(dir)
-
 result = []
-test_bar = tqdm(total=len(Artwork['id']))
-i=1
-# with Pool(processes=1, initializer=i1) as pool:
-for ids_and_urls in list(Artwork[['id','image_url']].itertuples(index=False, name=None)):
-    if len(result)==500:
-        save(result,'/media/stratos/New Volume/'+'img_pallets_'+str(i)+'.csv')
-        result = []
-        i+=1
+test_bar = tqdm(total=len(image_list))
+
+iterator = map(get_img_pallete, image_list)
+while True:
     test_bar.update()
     try:
-        result.append(get_img_pallete(ids_and_urls))
+        result.append(next(iterator))
     except StopIteration:
         break
     except Exception as e:
         print('Error at iteration', len(result))
         print(e)
 
-save(result,'/media/stratos/New Volume/img_pallets_last.csv')
+with open(dataset_dir+'img_pallets_faces.pkl', 'wb') as handle:
+    pickle.dump(result, handle)
