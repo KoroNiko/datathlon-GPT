71dac3de1e839d1561171bf6ce1dc340f4026dd0
KoroNiko <24572643+KoroNiko@users.noreply.github.com>
2023-02-28 10:44:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,18 +18,7 @@ app = dash.Dash(
 app.title = 'Datathlon - GPT'
 # app.layout = homepage_layout
 
-# server.config['SQLALCHEMY_DATABASE_URI'] = '''mysql+pymysql://gpt_admin:HN9gLBmeR3JiXS9@datathlondb.c8s8i66l2ado.us-east-1.rds.amazonaws.com/datathlonDB_main'''
-server.config['SQLALCHEMY_DATABASE_URI'] = '''mysql+pymysql://admin:XNQD6TkkB665b9p@last-database-gpt.c8s8i66l2ado.us-east-1.rds.amazonaws.com/main'''
-
-# DATABASES = {
-#     'NAME': os.environ['RDS_DB_NAME'],
-#     'USER': os.environ['RDS_USERNAME'],
-#     'PASSWORD': os.environ['RDS_PASSWORD'],
-#     'HOST': os.environ['RDS_HOSTNAME'],
-#     'PORT': os.environ['RDS_PORT'],
-# } 
-# print(DATABASES)
-# server.config['SQLALCHEMY_DATABASE_URI'] = f'''mysql+pymysql://{DATABASES['USER']}:{DATABASES['PASSWORD']}@{DATABASES['HOST']}/{DATABASES['NAME']}'''
+server.config['SQLALCHEMY_DATABASE_URI'] = '''mysql+pymysql://<username>:<password>@last-database-gpt.c8s8i66l2ado.us-east-1.rds.amazonaws.com/main'''
 
 db = SQLAlchemy()
 db.init_app(server)
